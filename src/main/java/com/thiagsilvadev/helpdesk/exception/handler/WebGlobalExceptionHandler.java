4a6108c2a265150eb2a74d09bbab2b13389916db
Thiago Santos Silva <109832582+ThiagSilvaDev@users.noreply.github.com>
2026-04-08 18:30:47 -0300

    refactor(exception): update WebGlobalExceptionHandler to simplify error handling and improve response structure

--- src/main/java/com/thiagsilvadev/helpdesk/exception/handler/WebGlobalExceptionHandler.java
+++ src/main/java/com/thiagsilvadev/helpdesk/exception/handler/WebGlobalExceptionHandler.java
@@ -2,16 +2,15 @@ package com.thiagsilvadev.helpdesk.exception.handler;
 
 import com.thiagsilvadev.helpdesk.exception.GlobalExceptionHandler;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import java.util.List;
-
 @RestControllerAdvice
-@Order(1)
+@Order(0)
 public class WebGlobalExceptionHandler extends GlobalExceptionHandler {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
@@ -20,15 +19,12 @@ public class WebGlobalExceptionHandler extends GlobalExceptionHandler {
         String message = methods == null || methods.length == 0
                 ? "Supported methods information is unavailable"
                 : "Supported methods: " + String.join(", ", methods);
-        List<String> errors = List.of(message);
-        return enrichProblemDetail(ex.getBody(), errors);
+        return createProblemDetail(HttpStatus.METHOD_NOT_ALLOWED, message);
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
     public ProblemDetail handleNoResourceFound(NoResourceFoundException ex) {
         String message = "Resource not found: " + ex.getMessage();
-        List<String> errors = List.of(message);
-        return enrichProblemDetail(ex.getBody(), errors);
+        return createProblemDetail(HttpStatus.NOT_FOUND, message);
     }
-
 }
